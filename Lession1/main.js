226538c565b21885eb6e5af1ff8feaf95b95c0e4
VinhDoan <clonevinh95@gmail.com>
2022-08-29 10:42:08 +0700

    vinh

---
+++ Lession1/main.js
@@ -0,0 +1,21 @@
+// var y = undefined; hanh dong truoc khi gan y = 7 
+
+
+// console.log(a) false => Cannot access 'a' before initialization
+
+var x = 10;
+var y = 7;
+console.log(x, y);
+
+// let/ const 
+const a = 0;
+
+
+// global variables, scope variables
+
+if (true) {
+    // let g = 0;
+    console.log(y)
+}
+
+// console.log(g) // g is not defined
