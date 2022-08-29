8389c9eee71a443c258796a5a2488598c4272f7b
VinhDoan <clonevinh95@gmail.com>
2022-08-29 11:20:13 +0700

    vinh

--- Lession1/main.js
+++ Lession1/main.js
@@ -44,3 +44,49 @@ const printFunction = () => {
 function print () {
 
 }
+
+// arrow functions luôn lấy context nói chung, hoặc gần nó nhất 
+
+const button = document.querySelector('#btn');
+console.dir(button);
+
+button.addEventListener('click', () => {
+    console.log(this)
+});
+button.addEventListener('click', function () {
+    console.log(this)
+}); // this ở đây sẽ lấy context thuộc về, gần nó nhất ở đây là button
+
+
+
+
+import { sub } from "./module.js";
+
+console.log(sub(3, 2));
+
+
+// spread &  Rest
+// spread ... 
+const newObject = {
+    name: 'Trong Vinh',
+    address: {
+        street: '372 Song Nhue'
+    }
+}
+
+const newObject1 = {...newObject}
+
+newObject1.name = 'Doan Trong Vinh'
+newObject.address.street = '372 Bo Tay Song Nhue'
+console.log(newObject)
+console.log(newObject1)
+// => 372 Song nhue bi thay doi vi su dung chung bo nho 
+
+
+const arr2 = ['1', '2', '3', '4', '5', '6']
+const arr3 = [...arr2]
+arr3.push('7')
+console.log(arr2)
+console.log(arr3)
+
+// => chỉ là shallow coppy ( coppy 1 tầng ) ko thể truy cập đc address
